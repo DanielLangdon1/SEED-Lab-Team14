bc29ba90d947ed12e2e2b4591a023cc0804080fb
tatemorrison22 <159830915+tatemorrison22@users.noreply.github.com>
2024-03-06 16:43:51 -0700

    Update demo1.py
    Added Calibration

--- Demo1/demo1.py
+++ Demo1/demo1.py
@@ -1,4 +1,12 @@
+#Max Allen and Tate Morrison
+#This code does the following:
+#Loads our calibrated Matrices
+#Takes video feed of calibrated photo
+#Checks for Aruco's
+#Calculates Angle of detected
 
+from numpy import savetxt
+from numpy import genfromtxt
 from time import sleep
 import numpy as np
 import cv2
@@ -10,9 +18,15 @@ import queue
 import threading
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
+#Loading in previously calculated matrix undistortion
+rvecs=genfromtxt('/home/seedlab/CalibrationImages/rotation_vecs.csv',delimiter=',')
+tvecs=genfromtxt('/home/seedlab/CalibrationImages/translation_vecs.csv',delimiter=',')
+mtx=genfromtxt('/home/seedlab/CalibrationImages/camera_matrix.csv',delimiter=',')
+dist=genfromtxt('/home/seedlab/CalibrationImages/camera_distortion.csv',delimiter=',')
 degPerPixel= 0.04664
 q=queue.Queue()
 
+#Writing to the LCD board and using threading to be efficient
 def LCDWrite():
     i2c = board.I2C()  # uses board.SCL and board.SDA
     i2d=SMBus(1)
@@ -38,38 +52,49 @@ myThread.start()
 
 aruco_dict=aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 
-
-
-
-
-	
 # initialize the camera. If channel 0 doesn't work, try channel 1
+# Setting cam properties and auto exposure
 camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
-
+camera.set(cv2.CAP_PROP_AUTO_EXPOSURE,.25)
+camera.set(cv2.CAP_PROP_EXPOSURE, -7)
 	
 # Let the camera warmup
 sleep(0.1)
 	
 # Get an image from the camera stream
 ret, image = camera.read()
-dummy=""
-last_state=0
-	
+
+#Find calibration matrix for this image
+h,w =image.shape[:2]
+newcameramtx,roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+#Checking if cam took photo
 if not ret:
 	print("Could not capture image from camera!")
 	quit()
 else:
+    # Cam succeeded
     while(1):
-        
-        grey=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        # Camera calibration / undistorton
+        dst=cv2.undistort(image,mtx,dist,None,newcameramtx)
+        x,y,w,h=roi
+        dst=dst[y:y+h,x:x+w]
+
+        # Detection of AruCo markers from greyscale
+        grey=cv2.cvtColor(dst, cv2.COLOR_RGB2GRAY)
         corners,ids,rejected = aruco.detectMarkers(grey,aruco_dict)
-        overlay = cv2.cvtColor(grey,cv2.COLOR_GRAY2BGR) # Convert back to RGB for imshow, as well as for the next step
-        overlay = aruco.drawDetectedMarkers(overlay,corners,borderColor = 4)
+
+        #Commented out -- These lines show video feed
+        #overlay = cv2.cvtColor(grey,cv2.COLOR_GRAY2BGR) # Convert back to RGB for imshow, as well as for the next step
+        #overlay = aruco.drawDetectedMarkers(overlay,corners,borderColor = 4)
+
+        #Feeding to LCD
         if ids is None:
             q.queue.clear()
         if not ids is None:
+                # Angle and distance calculations
                 FocalLength=(100*abs(corners[0][0][3][0]-corners[0][0][2][0])/6.6)
                 distance = (1091 * 6.6) / abs(corners[0][0][3][0]-corners[0][0][2][0])
                 #print(f"distance is {distance}")
@@ -77,18 +102,20 @@ else:
                 angle= degPerPixel *((corners[0][0][3][0]+corners[0][0][2][0])/2 - 640)
                 print(f"Angle is {angle}")
                 q.put(angle)
-                for (outline, id) in zip(corners, ids):
-                        markerCorners = outline.reshape((4,2))
-                        value=str(ids)
-                        if(value not in dummy): 
-                            dummy+=value
-                        overlay = cv2.putText(image, str(id),(int(markerCorners[0,0]), int (markerCorners[0,1]) - 15),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 0,0), 2)
+
+                #Commented out -- These lines show video feed
+                #for (outline, id) in zip(corners, ids):
+                #        markerCorners = outline.reshape((4,2))
+                #       value=str(ids)
+                #        overlay = cv2.putText(dst, str(id),(int(markerCorners[0,0]), int (markerCorners[0,1]) - 15),cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 0,0), 2)
                 
 
-                        
-        cv2.imshow("overlay",image)
+        #Commented out -- These lines show video feed              
+        #cv2.imshow("overlay",dst)
 
         ret, image = camera.read()
-        k=cv2.waitKey(1) & 0xFF
-        if k== ord('q'):
-                break
+
+        #Commented out -- These lines show video feed
+        #k=cv2.waitKey(1) & 0xFF
+        #if k== ord('q'):
+        #        break

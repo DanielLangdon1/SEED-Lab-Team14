c64abf7c1a757d42c8019238bb01fb2c325681ef
DanielLangdon1 <159584780+DanielLangdon1@users.noreply.github.com>
2024-03-11 16:13:11 -0600

    Update Demo1.ino

--- Demo1/Demo1.ino
+++ Demo1/Demo1.ino
@@ -1,3 +1,18 @@
+/*
+Names: Ian McGrath and Daniel Langdon
+Code: Demo 1
+Setup:
+Using the motor drive Polula Dual MC33926 Motor Driver shield, the Arduino Uno, and 
+a Voltage Monitor. Connect the Motor Driver onto the Arduino Uno, connect a battery to the
+voltage monitor and connect the monitor to the motor driver through the two pins. Then connect 
+the motors to the two pins next to voltage pins of the motor driver and the encoder wired to an interrupt pin 2 or 3 
+and digital pin 5 or 6 with the blue and green wired going to Vcc and ground pins on the motor driver. The motor sign pins 
+allow voltage to flow to the motor and the Voltage pins send the voltage to allow the motor to spin. 
+Code Usage:
+This code uses a PI controller to allow the wheels to travel a certain angle and distance and stop. You set these values in the setup called
+desiredPhi and desiredDis in radians and meters respectively and if you connect the arduino and upload the code it will turn that angle and drive that distance 
+*/
+
 #include <Wire.h>
 #define MY_ADDR 8
 #define PI 3.14159265358979323 // Defines irrational number PI for mathematical use later
@@ -28,8 +43,8 @@ int initialEncoderCount[2] = {0,0}; //Found in setup to check for change in coun
 float initialEncoderCountRad[2] = {0,0}; //Radian version of initial count
 float vel[2]; //Velocity found from timeelapsed and count/radian change
 float desiredVel[2]={0,0}; //Desired velocity to achieve in radian/second
-float Kp_pos = 7.95548607436073; //Controller gain for proportional 
-float Ki_pos = 0.5; //Controller gain for integrator
+float Kp_pos = 7.85; //Controller gain for proportional 
+float Ki_pos = 1.25; //Controller gain for integrator
 float Kp = 2.5;
 float voltage[2] = {0,0}; // voltage to be used for speed and position control
 float batteryVoltage = 7.8; //Sets saturation point for battery
@@ -40,50 +55,18 @@ float desiredPos[2]= {0,0};
 float integralError[2] = {0,0};
 float error[2] = {0,0};
 
-
-
 ///////position variables
 float b = 0.3556; //m between middles of wheels
 float r = 0.07; //wheel rad m
-float distanceTraveled = 0;
 int mode = 0; ///0 for turn 1 for move
 int count = 0;
 
-float xVel1 = 0;
-float xVel2 = 0;
-float xNew = 0;
-float yNew = 0;
-float xOld = 0;
-float yOld = 0;
 float distance[2] = {0,0};
-
 
 float phiNew = 0;
 float phiOld = 0;
-
 float desiredPhi = 0;
 float phiError = 0;
-float intErrorPhi = 0;
-float phiVelError = 0;
-float phiVel = 0;
-float desiredPhiVel = 0;
-float KpPhiPos = 7.025265;
-float KiPhiPos = .740405;
-float KpPhiVel = .213320;
-
-
-float xVel = 0;
-float yVel = 0;
-float desiredDis;
-float disError;
-float intErrorDis = 0;
-float forwardVel;
-float deltaV;
-float Vbar;
-float Vleft;
-float Vright;
-float disVelError;
-float desiredDisVel;
 
 
 int PWM[2] = {0,0}; //PWM variable to be used for later
@@ -119,18 +102,8 @@ void myISR2() {
 //Function to return encoder counts
 int MyEnc1() {
   if(digitalRead(encoderInterrupts[0]) != digitalRead(encoderPins[0])) {
-    // if (count1 >= 3200) {
-    //   count1-=3200;
-    // } else if (count1 <= -3200) {
-    //     count1 += 3200;
-    // }
     return(count1+1);
   } else{
-    //  if (count1 >= 3200) {
-    //   count1-=3200;
-    // } else if (count1 <= -3200) {
-    //     count1 += 3200;
-    // }
     return(count1);
   }
 }
@@ -138,18 +111,8 @@ int MyEnc1() {
 //Function to return encoder counts
 int MyEnc2() {
   if(digitalRead(encoderInterrupts[1]) != digitalRead(encoderPins[1])) {
-  // if (count1 >= 3200) {
-  //     count2-=3200;
-  //   } else if (count1 <= -3200) {
-  //       count2 += 3200;
-  //   }
     return(count2+1);
   } else{
-    //  if (count2 >= 3200) {
-    //   count2-=3200;
-    // } else if (count2 <= -3200) {
-    //     count2 += 3200;
-    // }
     return(count2);
   }
 }
@@ -182,8 +145,9 @@ void setup() {
   startTime = millis();
   initialTime = millis();
 
-  desiredPhi = PI/2;
-  desiredDis = 1.524; //M
+  // Variables for desired angle and distance to travel in radians and meters
+  desiredPhi = 5*PI/4-4*PI/180;
+  desiredDis = 0.85; //M
 
 }
 
@@ -196,19 +160,22 @@ void loop() {
   currentEncoderCount[0] = MyEnc1();
   currentEncoderCount[1] = MyEnc2();
 
-  for(int i = 0;i<2;i++){ // configure encoders
+  for(int i = 0;i<2;i++){ // set encoders to radians and finds velocity
     currentEncoderCountRad[i] = 2*PI*(float)(currentEncoderCount[i])/3200;
     vel[i] = (currentEncoderCountRad[i]-initialEncoderCountRad[i])/timeElapsed;
     distance[i] = currentEncoderCountRad[i]*r;
   }
+  // Finds current angle and angular velocity
   phiNew = (((initialEncoderCountRad[0]*r - distance[1]) + (initialEncoderCountRad[1]*r-distance[1]))/b);
   phiVel = (((vel[0]*r) - (vel[1]*r))/b);
+
+  //Sets old values to new values
   for(int i = 0;i<2;i++){ 
     initialEncoderCount[i] = currentEncoderCount[i];
     initialEncoderCountRad[i] = currentEncoderCountRad[i];
   }
-
   initialTime = millis();
+  // Switch mode to separate turning and moving forward code. Turn is mode 0 and then switched to mode 1 to go straight
   switch (mode){
     case 0:
       if (desiredPhi > 0){ ///calculate desired anglular position for each wheel based off of phi
@@ -218,14 +185,8 @@ void loop() {
         desiredPos[0] = -1*desiredPhi*(b/(2*r));  
         desiredPos[1] = desiredPhi*(b/(2*r));     
       }
-      Serial.print(phiNew/(PI/180));
-      Serial.print("\t");
-      Serial.println(phiError);
-      Serial.print("\t");
-      Serial.print(currentEncoderCount[0]);
-      Serial.print("\t");
-      Serial.println(currentEncoderCount[1]);
-      if(phiNew <= desiredPhi + PI/180 && phiNew >= desiredPhi - PI/180){
+      //Checks if within angular range and changes mode 
+      if(phiNew <= desiredPhi + PI/180 && phiNew >= desiredPhi - PI/180){ 
         mode = 1;
         delay(1000);
         PWM[0] = 0;
@@ -234,60 +195,45 @@ void loop() {
         count2 = 0;
       }
     break;
+
     case 1:
+    // Finds distance each wheel needs to travel to reach desired distance
       desiredPos[0] = desiredDis/(r);
       desiredPos[1] = desiredDis/(r);
       desiredPhi = 0;
-      Serial.print(voltage[0]);
-      Serial.print("\t");
-      Serial.println(voltage[1]);
-
-
+      
+      //Checks if position is reached and turns off motors. 
       if ((currentEncoderCountRad[0]*r <= desiredDis+0.0254 && currentEncoderCountRad[0]*r >= desiredDis - 0.0254) && (currentEncoderCountRad[1]*r <= desiredDis+0.0254 && currentEncoderCountRad[1]*r >= desiredDis - 0.0254) ) {
         PWM[0] = 0;
         PWM[1] = 0;
       }
     break;
-    case 3:
-      PWM[0] = 0;
-      PWM[1] = 0;
-    break;
   }
-  phiError = desiredPhi - phiNew;
-  if (abs(phiError) <= 0.1704 ){
-    
-  }
+
+  //Run PI controller for each wheel 
   for (int i = 0; i<2; i++) { // Calculate Porportional controler for each wheel
       pos_error[i] = desiredPos[i] - currentEncoderCountRad[i]; // calculate position error from desired pos
       integralError[i] = integralError[i] + pos_error[i]*((float)(desired_Ts_ms/1000)); // integral error
       desiredVel[i] = Kp_pos*pos_error[i] + Ki_pos * integralError[i]; // calculate desired velocity for each wheel with K values found in simulation
-      if (abs(desiredVel[i]) > 5) {
+      if (abs(desiredVel[i]) > 4) {
         if(desiredVel[i] < 0) {
-          desiredVel[i] = -5;
+          desiredVel[i] = -4;
         } else {
-          desiredVel[i] = 5;
+          desiredVel[i] = 4;
         }
       }
-      // if(vel[0] == vel[1] || mode == 0){ 
-      //   error[i] = desiredVel[i] - vel[i];}
-     error[i] = desiredVel[i] - vel[i];
-     voltage[i] = Kp * error[i];
-      // if((vel[0]>vel[1]) && (mode == 1)){
-      //   voltage[0] = voltage[0] - (voltage[0]-voltage[1])/2;
-      //   voltage[1] = voltage[1] + (voltage[0]-voltage[1])/2;
-      // }
-      // else if((vel[0]<vel[1]) && (mode == 1)){
-      //   voltage[1] = voltage[1] - (voltage[1]-voltage[0])/2;
-      //   voltage[0] = voltage[0] + (voltage[1]-voltage[0])/2;
-      // }
+      error[i] = desiredVel[i] - vel[i];
+      voltage[i] = Kp * error[i];
       if (voltage[i] >= 0) {
         digitalWrite(MotorSign[i],LOW);
       } else {
         digitalWrite(MotorSign[i], HIGH);   
       }
-        PWM[i] = 255*abs(voltage[i])/batteryVoltage; //run motors with calculated PID 
-        analogWrite(MotorVoltage[i],min(PWM[i],255));
-      }
+      PWM[i] = 255*abs(voltage[i])/batteryVoltage; //run motors with calculated PID 
+      analogWrite(MotorVoltage[i],min(PWM[i],255));
+  }
+
+
   while(millis()<last_time_ms+desired_Ts_ms){
     //wait till desired time passes
   }
